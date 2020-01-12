a831abec7e282b35ed5d3199290e597f32204399
PengpengDu <you@example.com>
2020-01-12 17:19:33 +0800

    first

---
+++ data1.js
@@ -0,0 +1,214 @@
+var data = {
+  features: [
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "chongming",
+        x: 121.409879,
+        y: 31.629061,
+        z: 99
+      },
+      geometry: {
+        x: 121.409879,
+        y: 31.629061
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "baoshan",
+        x: 121.490817,
+        y: 31.409294,
+        z: 70
+      },
+      geometry: {
+        x: 121.490817,
+        y: 31.409294
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "jiading",
+        x: 121.272591,
+        y: 31.379663,
+        z: 46
+      },
+      geometry: {
+        x: 121.272591,
+        y: 31.379663
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "zhabei",
+        x: 121.438336,
+        y: 31.291397,
+        z: 46
+      },
+      geometry: {
+        x: 121.438336,
+        y: 31.291397
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "hongkou",
+        x: 121.507561,
+        y: 31.270734,
+        z: 189
+      },
+      geometry: {
+        x: 121.507561,
+        y: 31.270734
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "yangpu",
+        x: 121.533992,
+        y: 31.265448,
+        z: 96
+      },
+      geometry: {
+        x: 121.533992,
+        y: 31.265448
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "pudong",
+        x: 121.673199,
+        y: 31.142083,
+        z: 100
+      },
+      geometry: {
+        x: 121.673199,
+        y: 31.142083
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "dishuihu",
+        x: 121.884631,
+        y: 30.923547,
+        z: 77
+      },
+      geometry: {
+        x: 121.884631,
+        y: 30.923547
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "putuo",
+        x: 121.422158,
+        y: 31.241753,
+        z: 165
+      },
+      geometry: {
+        x: 121.422158,
+        y: 31.241753
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "huangpu",
+        x: 121.490104,
+        y: 31.225616,
+        z: 146
+      },
+      geometry: {
+        x: 121.490104,
+        y: 31.225616
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "minhang",
+        x: 121.390341,
+        y: 31.118347,
+        z: 100
+      },
+      geometry: {
+        x: 121.390341,
+        y: 31.118347
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "fengxian",
+        x: 121.487705,
+        y: 30.921361,
+        z: 46
+      },
+      geometry: {
+        x: 121.487705,
+        y: 30.921361
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "songjiang",
+        x: 121.233326,
+        y: 31.040599,
+        z: 139
+      },
+      geometry: {
+        x: 121.233326,
+        y: 31.040599
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "jinshan",
+        x: 121.334511,
+        y: 30.7431,
+        z: 86
+      },
+      geometry: {
+        x: 121.334511,
+        y: 30.7431
+      }
+    },
+    {
+      attributes: {
+        FID: 0,
+        id: 1,
+        name: "qingpu",
+        x: 121.129401,
+        y: 31.155454,
+        z: 100
+      },
+      geometry: {
+        x: 121.129401,
+        y: 31.155454
+      }
+    }
+  ]
+};
